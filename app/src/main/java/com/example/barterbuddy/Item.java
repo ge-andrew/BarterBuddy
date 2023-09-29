40d85e9aa343cc252afc84091d5ed24c0f421a85
Skyler Atchison <skyler.atchison145@myci.csuci.edu>
2023-09-29 11:23:21 -0700

    Commented out some fields I think we won't need

--- app/src/main/java/com/example/barterbuddy/Item.java
+++ app/src/main/java/com/example/barterbuddy/Item.java
@@ -4,11 +4,13 @@ package com.example.barterbuddy;
  * This class is responsible for storing all data related to an item
  */
 public class Item {
-  // owner_id holds the user id used by firebase for teh owner of the item
-  private String owner_id;
-  // item_id holds the id used by Firestore
-  private String item_id;
-  // title holds the title of an the item
+  // owner_id holds the user id used by firebase for the owner of the item
+  // TODO: We may not need to store these ids since we can just use ".parent()" to get to the owner
+//  private String owner_id;
+//  // item_id holds the id used by Firestore
+  // TODO: This probably isn't a necessary field either
+//  private String item_id;
+//  // title holds the title of an the item
   private String title;
   // description holds the description of the item
   private String description;
@@ -18,13 +20,13 @@ public class Item {
   private boolean is_active;
 
   // getter methods
-  public String getOwner_id() {
-    return owner_id;
-  }
-
-  public String getItem_id() {
-    return item_id;
-  }
+//  public String getOwner_id() {
+//    return owner_id;
+//  }
+//
+//  public String getItem_id() {
+//    return item_id;
+//  }
 
   public String getTitle() {
     return title;
@@ -43,13 +45,13 @@ public class Item {
   }
 
   // setter methods
-  public void setOwner_id(String owner_id) {
-    this.owner_id = owner_id;
-  }
-
-  public void setItem_id(String item_id) {
-    this.item_id = item_id;
-  }
+//  public void setOwner_id(String owner_id) {
+//    this.owner_id = owner_id;
+//  }
+//
+//  public void setItem_id(String item_id) {
+//    this.item_id = item_id;
+//  }
 
   public void setTitle(String title) {
     this.title = title;
@@ -69,14 +71,14 @@ public class Item {
 
   // constructors
   public Item(
-      String owner_id,
-      String item_id,
+//      String owner_id,
+//      String item_id,
       String title,
       String description,
       String image_id,
       boolean is_active) {
-    this.owner_id = owner_id;
-    this.item_id = item_id;
+//    this.owner_id = owner_id;
+//    this.item_id = item_id;
     this.title = title;
     this.description = description;
     this.image_id = image_id;
