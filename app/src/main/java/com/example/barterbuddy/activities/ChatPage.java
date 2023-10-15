f76c0ea71f7f1d815e8d632a839c332979bb01bb
Skyler Atchison <skyler.atchison145@myci.csuci.edu>
2023-10-14 18:19:00 -0700

    started work on chat activity (new resources, etc.)

---
+++ app/src/main/java/com/example/barterbuddy/activities/ChatPage.java
@@ -0,0 +1,16 @@
+package com.example.barterbuddy.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.barterbuddy.R;
+
+public class ChatPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_page);
+    }
+}
