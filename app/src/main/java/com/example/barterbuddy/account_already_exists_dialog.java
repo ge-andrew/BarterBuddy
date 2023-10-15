9dd55170932becf7a934684f895eb1f527ac5f52
Daniel Peace <danielpeace805@gmail.com>
2023-10-15 13:27:35 -0700

    - Added functionality to allow a user to register an account. This not only generates an account in Firebase Authentication, but also saves their data in Firestore in case it is needed. - The page also checks if any of the fields are empty and shows a toast letting the user know. - Created an activity for a dialogue box for if an account already exists, this is not fully implemented yet.

---
+++ app/src/main/java/com/example/barterbuddy/account_already_exists_dialog.java
@@ -0,0 +1,13 @@
+package com.example.barterbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class account_already_exists_dialog extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_account_already_exists_dialog);
+  }
+}
---
+++ app/src/main/java/com/example/barterbuddy/activities/CreateAccountPage.java
@@ -0,0 +1,146 @@
+package com.example.barterbuddy.activities;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.barterbuddy.R;
+import com.example.barterbuddy.models.User;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class CreateAccountPage extends AppCompatActivity {
+
+  // declaring firebase variables
+  private final FirebaseFirestore DATABASE_INSTANCE = FirebaseFirestore.getInstance();
+  private final FirebaseAuth AUTHENTICATION_INSTANCE = FirebaseAuth.getInstance();
+  DocumentReference userReference;
+  FirebaseAuth userAuthentication;
+
+  // declaring variables for holding user data
+  private String username;
+  private String email;
+  private String password;
+
+  // declaring views
+  TextView loginTextView;
+  TextInputEditText usernameEditText;
+  TextInputEditText emailEditText;
+  TextInputEditText passwordEditText;
+  Button createButton;
+  CheckBox showPasswordCheckBox;
+  ImageView backArrow;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_create_account_page);
+
+    // initializing authenticator variable
+    userAuthentication = AUTHENTICATION_INSTANCE;
+
+    // find views
+    loginTextView = findViewById(R.id.login_text_view);
+    usernameEditText = findViewById(R.id.username_text_field);
+    emailEditText = findViewById(R.id.email_text_field);
+    passwordEditText = findViewById(R.id.password_text_field);
+    createButton = findViewById(R.id.create_button);
+    showPasswordCheckBox = findViewById(R.id.show_password_checkbox);
+    backArrow = findViewById(R.id.back_arrow);
+
+    // change the visibility status of the password field
+    showPasswordCheckBox.setOnClickListener(
+        view -> {
+          if (showPasswordCheckBox.isChecked()) {
+            // setting password to visible
+            passwordEditText.setTransformationMethod(null);
+          } else {
+            // setting password to invisible
+            passwordEditText.setTransformationMethod(new PasswordTransformationMethod());
+          }
+        });
+
+    // save account and save data in database
+    createButton.setOnClickListener(
+        view -> {
+          // getting account information from input fields
+          username = String.valueOf(usernameEditText.getText());
+          email = String.valueOf(emailEditText.getText());
+          password = String.valueOf(passwordEditText.getText());
+
+          // create user object to store data
+          User newUser = new User(username, email, password);
+
+          if (missingUserInfo(newUser)) {
+            return;
+          }
+
+          userAuthentication
+              .createUserWithEmailAndPassword(email, password)
+              .addOnCompleteListener(
+                  task -> {
+                    if (task.isSuccessful()) {
+                      Toast.makeText(CreateAccountPage.this, "Account created!", Toast.LENGTH_SHORT)
+                          .show();
+
+                    } else {
+                      // If sign in fails, display a message to the user.
+                      Toast.makeText(
+                              CreateAccountPage.this, "Authentication failed.", Toast.LENGTH_SHORT)
+                          .show();
+                    }
+                  });
+
+          // create reference to new user
+          userReference = DATABASE_INSTANCE.collection("users").document(newUser.getEmail());
+
+          // store user data in Firestore
+          userReference.set(newUser);
+        });
+
+    // goes back to login page
+    backArrow.setOnClickListener(view -> finish());
+
+    // goes back to login page
+    loginTextView.setOnClickListener(view -> finish());
+  }
+
+  /**
+   * This method takes a user object and checks if any of the members are empty
+   *
+   * @param user
+   * takes a user object containing a user's account information
+   * @return boolean
+   */
+  public boolean missingUserInfo(User user) {
+    // check if all information was provided
+    if (TextUtils.isEmpty(user.getUsername()) && TextUtils.isEmpty(user.getEmail())) {
+      Toast.makeText(CreateAccountPage.this, "Missing information", Toast.LENGTH_SHORT).show();
+      return true;
+    } else if (TextUtils.isEmpty(user.getEmail()) && TextUtils.isEmpty(user.getPassword())) {
+      Toast.makeText(CreateAccountPage.this, "Missing information", Toast.LENGTH_SHORT).show();
+      return true;
+    } else if (TextUtils.isEmpty(user.getUsername()) && TextUtils.isEmpty(user.getPassword())) {
+      Toast.makeText(CreateAccountPage.this, "Missing information", Toast.LENGTH_SHORT).show();
+      return true;
+    } else if (TextUtils.isEmpty(user.getUsername())) {
+      Toast.makeText(CreateAccountPage.this, "Missing username", Toast.LENGTH_SHORT).show();
+      return true;
+    } else if (TextUtils.isEmpty(user.getEmail())) {
+      Toast.makeText(CreateAccountPage.this, "Missing email", Toast.LENGTH_SHORT).show();
+      return true;
+    } else if (TextUtils.isEmpty(user.getPassword())) {
+      Toast.makeText(CreateAccountPage.this, "Missing password", Toast.LENGTH_SHORT).show();
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
