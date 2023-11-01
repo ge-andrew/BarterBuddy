c715b8b0d902a5943384eeb32b6b39098cc4fdb7
Skyler Atchison <skyler.atchison145@myci.csuci.edu>
2023-11-01 10:00:34 -0700

    Made progress on local testing

--- app/src/androidTest/java/com/example/barterbuddy/AddNewItemInstrumentedTest.java
+++ app/src/androidTest/java/com/example/barterbuddy/AddNewItemInstrumentedTest.java
@@ -2,13 +2,14 @@ package com.example.barterbuddy;
 
 import static org.junit.Assert.*;
 
-import android.content.Context;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.example.barterbuddy.models.Item;
+import com.example.barterbuddy.models.User;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,23 +18,40 @@ import org.junit.runner.RunWith;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-
 @RunWith(AndroidJUnit4.class)
-public class AddNewItemInstrumentedTest { 7
+public class AddNewItemInstrumentedTest {
+  private FirebaseFirestore localFirestore;
+
+  private User userAndrew;
+  private Item waterBottle;
+
+  @Before
+  public void setUpFirestoreInstance() {
     // 10.0.2.2 is the special IP address to connect to the 'localhost' of
     // the host computer from an Android emulator.
-    FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-    firestore.useEmulator("10.0.2.2", 8080);
+    localFirestore = FirebaseFirestore.getInstance();
+    localFirestore.useEmulator("10.0.2.2", 8080);
 
-    FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-            .setPersistenceEnabled(false)
-            .build();
-    firestore.setFirestoreSettings(settings);
+    FirebaseFirestoreSettings settings =
+        new FirebaseFirestoreSettings.Builder().setPersistenceEnabled(false).build();
+    localFirestore.setFirestoreSettings(settings);
+  }
+
+  @Before
+  public void populateLocalFirestoreWithData() {
+    userAndrew = new User("Andrew Ge", "andrew@google.com", "password");
+    waterBottle =
+        new Item(
+            "Insulated Water Bottle",
+            "One dent!",
+            "andrew@google.com-Insulated Water Bottle",
+            false,
+            "Andrew Ge",
+            "andrew@google.com");
+  }
 
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.barterbuddy", appContext.getPackageName());
+    public void addScissors() {
+
     }
 }
