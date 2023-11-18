d6efa940b635cd7364cf91ed4dd69134aee8c088
Daniel Peace <danielpeace805@gmail.com>
2023-11-18 10:46:16 -0800

    - created base UI for profile page

---
+++ app/src/main/java/com/example/barterbuddy/activities/ProfilePage.java
@@ -0,0 +1,14 @@
+package com.example.barterbuddy.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.barterbuddy.R;
+
+public class ProfilePage extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_profile_page);
+  }
+}
