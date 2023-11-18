42124d50ed7789287ad6b02b23809bd48c0ba783
Daniel Peace <danielpeace805@gmail.com>
2023-11-18 11:16:26 -0800

    Added you UI and backend for displaying a profile. THe backend for the rating system still needs to be added

--- app/src/main/java/com/example/barterbuddy/activities/MainActivity.java
+++ app/src/main/java/com/example/barterbuddy/activities/MainActivity.java
@@ -76,6 +76,11 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         }
+        else if(id == R.id.profile) {
+            Intent intent = new Intent(getApplicationContext(), ProfilePage.class);
+            startActivity(intent);
+        }
+
         return true;
     }
 }
